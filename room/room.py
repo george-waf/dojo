fdb7f3878abab19f4cc1f9d84369b9a77945beca
george-waf <georgewafulan@gmail.com>
2017-04-11 10:11:38 +0300

    Update room.py

--- room/room.py
+++ room/room.py
@@ -14,27 +14,6 @@ class LivingSpace(Room):
         self.room_type = 'livingspace'
         self.max_space = 4
         
-import unittest
-class TestCreateRoom(unittest.TestCase):
-		
-	def setUp(self):
-		self.office_space = Office("R")
-		self.living_space = LivingSpace("Emerald")
-	
-	def test_officespace_size(self):
-		self.assertEqual(self.office_space.max_space, 6)
-			
-	def test_officespace_type(self):
-		self.assertEqual(self.office_space.room_type, "office")
-			
-	def test_livingspace_size(self):
-		self.assertEqual(self.living_space.max_space, 4) 
-			
-	def test_livingspace_type(self):
-		self.assertEqual(self.living_space.room_type, "livingspace")
-				
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)  
 
      
