2b96ef50014ec3c53b83e841a081c1f85d9268e4
george-waf <georgewafulan@gmail.com>
2017-04-12 14:13:16 +0300

    Update room.py

--- room/room.py
+++ room/room.py
@@ -1,18 +1,22 @@
 class Room(object):
-    def __init__(self, name):
-        self.name = name
-	   
+	
+	def __init__(self, name):
+		self.name = name
+		
 class Office(Room):
-    def __init__(self, name):
-        self.name = name
-        self.room_type = 'office'
-        self.max_space = 6
-        
+	
+	def __init__(self, name):
+		super(Office, self).__init__(name)
+		self.room_type = "office"
+		self.capacity = 6	
+		
 class LivingSpace(Room):
-    def __init__(self, name):
-        self.name = name
-        self.room_type = 'livingspace'
-        self.max_space = 4
+	
+	def __init__(self, name):
+		super(LivingSpace, self).__init__(name)
+		self.room_type = "living space"
+		self.capacity = 4
+
         
 
 
