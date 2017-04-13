6b7dc2fba516014198c5f2841fe5a51b39033051
george-waf <georgewafulan@gmail.com>
2017-04-13 10:23:56 +0300

    Update person.py

--- person/person.py
+++ person/person.py
@@ -1,23 +1,21 @@
 class Person(object):
 	
-	def __init__(self,first_name):
+	def __init__(self,first_name,second_name):
 		self.name = first_name
 		
 
 
 class Staff(Person):
 	
-	def __init__(self, first_name, second_name):
-		super(Staff, self).__init__(first_name)
+	def __init__(self, first_name,second_name):
+		super(Staff, self).__init__(first_name,second_name)
 		self.position = "staff"
 		self.wants_accomodation = "n"
 		
 
 class Fellow(Person):
 	
-	def __init__(self, first_name,  wants_accomodation="N"):
+	def __init__(self, first_name,second_name, wants_accomodation="n"):
 		super(Fellow, self).__init__(first_name,second_name)
 		self.position = "fellow"
 		self.wants_accomodation = wants_accomodation
-
-
